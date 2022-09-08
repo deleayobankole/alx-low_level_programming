24a9f3db2528d632103a17225c6ca54b297b902a
Ayo Bankole <deleayobankole@gmail.com>
2022-09-08 14:40:39 -0700

    7. Intel Assembly Code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,6 @@
  */
 int main(void)
 {
-	char MyString[] = "\"Programming is like building a multilingual puzzle\n";
-	puts(MyString);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
