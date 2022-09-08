425a6c6063246d80b83f7532377be54bc4a96789
Ayo Bankole <deleayobankole@gmail.com>
2022-09-08 10:15:30 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * This function will display a string of characters.
+ * Using the puts() function
+ *
+ */
+
+int main(void)
+{
+	char MyString[] = "\"Programming is like building a multilingual puzzle";
+	puts(MyString);
+
+	return 0;
+}
