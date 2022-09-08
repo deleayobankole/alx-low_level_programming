e8e847d2e719835d6aef378c94d887e7fa3c9012
Ayo Bankole <deleayobankole@gmail.com>
2022-09-08 11:44:43 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - the code begins here
+ *
+ * Return: always works if zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
