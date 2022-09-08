18a6c17cb0e0a11a443906f07e57b4543507c719
Ayo Bankole <deleayobankole@gmail.com>
2022-09-08 15:37:27 -0700

    8. Working with write() function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - it all begins here
+ *
+ * Return: this will return 1 if successful
+ *
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
