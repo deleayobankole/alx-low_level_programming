7ac8364682a512f1e77858f60f29e9ddeae62ace
Ayo Bankole <deleayobankole@gmail.com>
2022-09-09 14:37:20 -0700

    2. Print alphabets with putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - the code begins here
+ *
+ * Return: this will work if 0
+ *
+ */
+int main(void)
+{
+	char xter;
+
+	for (xter = 'a'; xter <= 'z'; ++xter)
+	{
+		putchar(xter);
+	}
+	putchar('\n');
+	return (0);
+}
