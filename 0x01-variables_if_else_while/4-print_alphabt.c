c70a41fcefc93947a188eec0d878d4e1ac61902f
Ayo Bankole <deleayobankole@gmail.com>
2022-09-09 15:37:53 -0700

    4. Skip two alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - the code begins here
+ *
+ * Return: this will work if 0
+ *
+ */
+int main(void)
+{
+	char xter = 'a';
+
+	do {
+		if (xter == 'e' || xter == 'q')
+		{
+			xter++;
+			continue;
+		}
+
+		putchar(xter);
+		xter++;
+
+	} while (xter < 'z');
+
+	putchar('\n');
+	return (0);
+}
