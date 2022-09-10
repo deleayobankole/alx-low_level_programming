df9c8045a38be05a9f91df90cd37c9784274fbde
Ayo Bankole <deleayobankole@gmail.com>
2022-09-09 20:24:27 -0700

    Combinations of two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - it all begins here
+ *
+ * Return: this works if 0
+ *
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 100; a++)
+	{
+		for (b = 0; b < 100; b++)
+		{
+			putchar(a / 10 + '0');
+			putchar(a % 10 + '0');
+			putchar(' ');
+			putchar(b / 10 + '0');
+			putchar(b % 10 + '0');
+
+			if (!(a == 98 && b == 99))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
