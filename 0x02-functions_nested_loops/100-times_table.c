2c9635a3aec0ddbc0ac80196462e027c0ae6d88e
Ayo Bankole <deleayobankole@gmail.com>
2022-09-14 05:32:57 -0700

    12. Print tables n times v.3

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,42 +8,39 @@
 void print_times_table(int n)
 {
 	int a, b, c;
-	
+
 	if (n > 15 || n < 0)
 		return;
-	
 	for (a = 0; a <= n; a++)
 	{
 		for (b = 0; b <= n; b++)
 		{
 			c = a * b;
 
-			if ((c / 10) == 0)
+			if (b == 0)
+				_putchar('0');
+			else if (c < 10)
 			{
-				if (b == 0)
-				{
-					_putchar('0');
-				}
-				if (b != 0)
-				{
-					_putchar(' ');
-					_putchar((c % 10) + '0');
-				}
-				if (b < n)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(c + '0');
+			}
+			else if (c < 100)
+			{
+				_putchar(' ');
+				_putchar('0' + c / 10);
+				_putchar('0' + c % 10);
 			}
 			else
 			{
-				_putchar((c / 10) + '0');
+				_putchar((c / 100) + '0');
+				_putchar('0' + (c - 100) / 10);
 				_putchar((c % 10) + '0');
-				if (b < n)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
+			}
+			if (b < n)
+			{
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
 		_putchar('\n');
