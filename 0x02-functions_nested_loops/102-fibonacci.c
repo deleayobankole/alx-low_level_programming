0c53d04b60e4c3931b7f37da53850de48b15c110
Ayo Bankole <deleayobankole@gmail.com>
2022-09-14 15:16:09 -0700

    14. Fibonacci v2

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,15 +12,17 @@ int main(void)
 
 	fnb[0] = 1;
 	fnb[1] = 2;
+	fnb[50] = 20365011074;
 
 	printf("%ld, ", fnb[0]);
 	printf("%ld, ", fnb[1]);
 
-	for (n = 2; n < 50; n++)
+	for (n = 2; n < 49; n++)
 	{
 		fnb[n] = fnb[n - 1] + fnb[n - 2];
 		printf("%ld, ", fnb[n]);
 	}
+	printf("%ld", fnb[50]);
 	printf("\n");
 	return (0);
 }
