dfcd4d39e9d84dff7c29b58a8f0cd297241fcfa6
Ayo Bankole <deleayobankole@gmail.com>
2022-09-15 07:09:31 -0700

    3. Remaining days in the year

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -28,7 +28,11 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else
+	else if (c > a && a > b)
+	{
+		largest = c;
+	}
+	else if (c > a && c > b)
 	{
 		largest = c;
 	}
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -9,4 +9,10 @@ void positive_or_negative(int i);
 /*this function finds the largest of 3 values*/
 int largest_number(int a, int b, int c);
 
+/* function evaluates days left in the year */
+int convert_day(int month, int day);
+
+/* function to calculate days laeft in the year*/
+void print_remaining_days(int month, int day, int year);
+
 #endif /* MAIN_H */
