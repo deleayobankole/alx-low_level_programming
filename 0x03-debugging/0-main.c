e4d6d0cd6d02616c470fe566825189feb6a225e7
Ayo Bankole <deleayobankole@gmail.com>
2022-09-15 01:32:50 -0700

    0. Testing multiple main function 3

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,11 @@
 #include "main.h"
 /**
  * main - tests function that prints if integer is positive or negative
+ * @i: parameter to test
  * Return: 0
  */
+void positive_or_negative(int i);
+
 int main(void)
 {
 	int i;
