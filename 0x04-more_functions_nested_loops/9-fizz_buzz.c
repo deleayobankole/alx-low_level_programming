908dfe403b00d5afeb15acbf5ab5babd45ea1941
Ayo Bankole <deleayobankole@gmail.com>
2022-09-15 16:17:43 -0700

    9. Fizz Buzz v3

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@ int main(void)
 	for (i = 1; i < 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			printf("Fizz Buzz ");
+			printf("FizzBuzz ");
 		else if (i % 3 == 0)
 			printf("Fizz ");
 		else if (i % 5 == 0)
