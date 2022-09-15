d0ba9799108c5461ebb2985cf5d35459c3d5b7db
Ayo Bankole <deleayobankole@gmail.com>
2022-09-15 02:55:53 -0700

    2. Largest numbers

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,10 @@
 
 #include <stdio.h>
 
+/*this function evaluates positive or negative values*/
 void positive_or_negative(int i);
+
+/*this function finds the largest of 3 values*/
+int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */
