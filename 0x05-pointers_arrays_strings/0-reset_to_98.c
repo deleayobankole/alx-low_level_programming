8cbdd99732e965d34418f0efd967bc1fe57d8229
Ayo Bankole <deleayobankole@gmail.com>
2022-09-19 06:55:32 -0700

    0. Reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset the value of n
+ * @n: set the value to 98
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,4 +4,7 @@
  * The header functions will be documented here
  */
 
+/* function to take and update pointer */
+void reset_to_98(int *n);
+
 
