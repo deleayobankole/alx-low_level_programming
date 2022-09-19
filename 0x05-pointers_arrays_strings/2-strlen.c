28d103b60167a415d8cc26a840b0ee6f94c791f6
Ayo Bankole <deleayobankole@gmail.com>
2022-09-19 11:05:11 -0700

    2. String Length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - lenght of string
+ * @s: tester
+ * Return: this is true
+ */
+
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; a < s[a];)
+	{
+		a++;
+	}
+
+	return (a);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,3 +10,6 @@ void reset_to_98(int *n);
 /* function that swaps the values of two integers */
 void swap_int(int *a, int *b);
 
+/* function that returns the length of a string */
+int _strlen(char *s);
+
