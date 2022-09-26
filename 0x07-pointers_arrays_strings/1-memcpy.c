f1ab9729beec0adef1e19e72a5ce4d85148cd1d3
Ayo Bankole <deleayobankole@gmail.com>
2022-09-26 11:09:37 -0700

    1. memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memcpy - fn def
+ * @dest: destination
+ * @src: source
+ * @n: integer to set
+ * Return: destination
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	if (dest == NULL)
+		return (NULL);
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /* function that fills memory with a constant byte */
 char *_memset(char *s, char b, unsigned int n);
