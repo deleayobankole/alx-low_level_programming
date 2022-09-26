5f4cbf8cf56aad6c07846f883f682181aeff2db5
Ayo Bankole <deleayobankole@gmail.com>
2022-09-26 11:12:06 -0700

    1. memcpy

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,4 +4,5 @@
 /* function that fills memory with a constant byte */
 char *_memset(char *s, char b, unsigned int n);
 
-
+/* function that copies memory area */
+char *_memcpy(char *dest, char *src, unsigned int n);
