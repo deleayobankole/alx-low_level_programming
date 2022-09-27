a6143677530e91e2658bfa9b5ae77039cd05fbc2
Ayo Bankole <deleayobankole@gmail.com>
2022-09-26 20:39:05 -0700

    5. find a a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strstr - fn to locate string
+ * @haystack: string to test
+ * @needle: substring to locate
+ * Return: sub or null
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	if (needle[0] == '\0')
+		return (haystack);
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] != needle[j])
+					break;
+			}
+			if (nedle[j] == '\0')
+				return (haystack + i);
+		}
+	}
+	return ('\0');
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -15,3 +15,8 @@ unsigned int _strspn(char *s, char *accept);
 
 /* function that searches a string for any of a set of bytes */
 char *_strpbrk(char *s, char *accept);
+
+/* function that locates a substring */
+char *_strstr(char *haystack, char *needle);
+
+
