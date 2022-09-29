bea8acd672446f0178372db51fe9ac1c6a305e26
Ayo Bankole <deleayobankole@gmail.com>
2022-09-28 20:25:48 -0700

    0. Print a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - fn to print a string
+ * @s: string to print
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,9 @@
 #include <stdio.h>
 
 /* the function prototypes reside here */
+
+/* print a character */
+int _putchar(char c);
+
+/* function that prints a string, followed by a new line. */
+void _puts_recursion(char *s);
