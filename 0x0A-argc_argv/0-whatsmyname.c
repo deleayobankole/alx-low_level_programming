c147c453c6b58a02c60761c3b283a11f4de41c7a
Ayo Bankole <deleayobankole@gmail.com>
2022-09-30 21:24:40 -0700

    0. Returns name of argument

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - the main fn
+ * @argc: argument count
+ * @argv: arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
