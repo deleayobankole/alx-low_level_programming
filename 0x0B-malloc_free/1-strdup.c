c37b78f839c02e1fb270663bd627cb2003cd7ddf
Ayo Bankole <deleayobankole@gmail.com>
2022-10-04 09:30:13 -0700

    1. return pointer to new space v3

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,32 +2,64 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+ * _strlen - count array
+ * @s: array elements
+ * Return: 1
+ */
+int _strlen(char *s)
+{
+	unsigned int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * _srtcpy - copy arays
+ * @src: array elements
+ * @dest: dest array
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+/**
  * _strdup - return to nelwy allocated space
  * @str: string
  * Return: string or null
  */
 char *_strdup(char *str)
 {
-	unsigned int i = 0, size = 0;
-	char *m;
+	char *dst;
+	unsigned int size;
 
-	if (str == NULL)
-		return (NULL);
-
-	for (; str[size]; size++)
-		;
-	size++;
-
-	m = malloc(size * sizeof(char));
-
-	if (m == NULL)
+	if (str == 0)
 	{
 		return (NULL);
 	}
-	else
+
+	size = _strlen(str) + 1;
+
+	dst = (char *) malloc(size * sizeof(char));
+
+	if (dst == 0)
 	{
-		for (; i < size; i++)
-			m[i] = str[i];
+		return (NULL);
 	}
-	return (m);
+	_strcpy(dst, str);
+	return (dst);
 }
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,24 @@
-/* main.h header file */
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _putchar(char);
+
 /* function that creates an array of chars, and initializes it with a specific char  */
 char *create_array(unsigned int size, char c)
 
 /* function that returns a pointer to a newly allocated space in memory  */	
 char *_strdup(char *str);	
+
+char *str_concat(char *s1, char *s2);
+
+int **alloc_grid(int width, int height);
+
+void free_grid(int **grid, int height);
+
+char *argstostr(int ac, char **av);
+
+char **strtow(char *str);
+
+
+
+#endif /* _MAIN_H_ */
