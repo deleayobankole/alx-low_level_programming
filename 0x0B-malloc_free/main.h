c19952e6de4b17062121911bea9703a95a89362f
Ayo Bankole <deleayobankole@gmail.com>
2022-10-04 10:37:34 -0700

    Fixed error in main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,7 +7,7 @@
 int _putchar(char);
 
 /* function that creates an array of chars, and initializes it with a specific char  */
-char *create_array(unsigned int size, char c)
+char *create_array(unsigned int size, char c);
 
 /* function that returns a pointer to a newly allocated space in memory  */	
 char *_strdup(char *str);	
