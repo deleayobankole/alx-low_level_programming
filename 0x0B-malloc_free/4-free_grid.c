fd1bf4ca3b2f95a6f342647ec3edc90691fb8937
Ayo Bankole <deleayobankole@gmail.com>
2022-10-04 11:06:54 -0700

    4. free grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * free_grid - frees up the grid
+ * @grid: grid to check
+ * @height: height of grid
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (height <= 0)
+		return;
+
+	if (grid == NULL)
+		return;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i];)
+	}
+	free(grid);
+}
