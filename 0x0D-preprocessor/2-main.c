2e64fcaf1b65416ce3c1e49d13c06824d995b049
Ayo Bankole <deleayobankole@gmail.com>
2022-10-10 02:55:02 -0700

    prints the name of the file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the file program was compiled from
+ * Return: 0 if success
+ */
+int main(void)
+{
+
+	printf("%s\n", _FILE_);
+	return (0);
+}
