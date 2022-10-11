00d416220ffd79e6c7035653ebf8ded3daf1979a
Ayo Bankole <deleayobankole@gmail.com>
2022-10-11 04:16:57 -0700

    0. struct elements

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,11 +12,9 @@ struct dog
 	float age;
 	char *owner;
 }
-/**
- * dog_t - Typedef for struct dog
- */
-typedef struct dog dog_t;
 
+typedef struct dog dog_t;
+int _putchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
