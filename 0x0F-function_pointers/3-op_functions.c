11492dcd4f66714696b29ff7c4a4259e1afb1392
Ayo Bankole <deleayobankole@gmail.com>
2022-10-12 15:21:59 -0700

    3. Perform simple operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * op_add - adds 5 functions
+ * @a: int a
+ * @b: int b
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtracts
+ * @a: int a
+ * @b: int b
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies
+ * @a: int a
+ * @b: int b
+ * Return: multiply
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - division
+ * @a: int a
+ * @b: int b
+ * Return: division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - modulos
+ * @a: int a
+ * @b: int b
+ * Return: modulus
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
